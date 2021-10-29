1d4b3688841f1f3404502f795f7f58325b213b9d
Danielle Sisserman <sisse008@umn.edu>
2021-10-29 13:19:27 +0300

    updated gitignore file to ignore libraru folder

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MVCController : MVCAbstract
+public class GameController : MVCAbstract
 {
    
     public UIMenuController UIMenu;
